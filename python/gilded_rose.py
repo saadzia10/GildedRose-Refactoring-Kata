adf0bc8015d45c2f4130549906ba59e395c520ed
Saad Zia <msaadzia7@gmail.com>
2025-07-22 14:33:40 +0100

    Added concrete updaters for items, accessible by mappers

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,55 +1,28 @@
 # -*- coding: utf-8 -*-
+from updaters import StandardUpdater, AgedBrieUpdater, BackstagePassesUpdater, SulfurasUpdater
 
-MAX_QUALITY = 50
-MIN_QUALITY = 0
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 AGED_BRIE = "Aged Brie"
 BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 
-def clamp_quality(quality):
-    return max(MIN_QUALITY, min(quality, MAX_QUALITY))
+UPDATERS = {
+    SULFURAS: SulfurasUpdater(),
+    AGED_BRIE: AgedBrieUpdater(),
+    BACKSTAGE_PASSES: BackstagePassesUpdater(),
+}
 
-def increment_quality(item, amount=1):
-    item.quality = clamp_quality(item.quality + amount)
+def get_updater(item):
+    return UPDATERS.get(item.name, StandardUpdater())
 
-def decrement_quality(item, amount=1):
-    item.quality = clamp_quality(item.quality - amount)
 
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def _update_standard_item(self, item, expired):
-        decrement_quality(item, 2 if expired else 1)
-
-    def _update_aged_brie(self, item, expired):
-        increment_quality(item, 2 if expired else 1)
-    
-    def _update_backstage_passes(self, item, expired):
-        
-        if expired:
-            item.quality = 0
-        else:
-            if item.sell_in < 6:
-                increment_quality(item, 3)
-            elif item.sell_in < 11:
-                increment_quality(item, 2)
-            else:
-                increment_quality(item, 1)
-
     def update_quality(self):
         for item in self.items:
-            if item.name == SULFURAS:
-                continue
-            elif item.name == AGED_BRIE:
-                self._update_aged_brie(item, item.sell_in <= 0)
-            elif item.name == BACKSTAGE_PASSES:
-                self._update_backstage_passes(item, item.sell_in <= 0)
-            else:
-                self._update_standard_item(item, item.sell_in <= 0)
-
-            item.sell_in -= 1
+            get_updater(item).update(item)
 
 
 class Item:
---
+++ python/updaters.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+MIN_QUALITY = 0
+MAX_QUALITY = 50
+
+
+class Updater(ABC):
+    @abstractmethod
+    def update(self, item):
+        pass
+
+    def _clamp(self, value, min_value, max_value):
+        return max(min_value, min(value, max_value))
+
+    def _inc(self, item, amount=1):
+        item.quality = self._clamp(item.quality + amount, MIN_QUALITY, MAX_QUALITY)
+
+    def _dec(self, item, amount=1):
+        item.quality = self._clamp(item.quality - amount, MIN_QUALITY, MAX_QUALITY)
+
+class StandardUpdater(Updater):
+    def update(self, item):
+        change = 2 if item.sell_in <= 0 else 1
+        self._dec(item, change)
+        item.sell_in -= 1
+
+
+class SulfurasUpdater(Updater):
+    def update(self, item):
+        pass
+
+class AgedBrieUpdater(Updater):
+    def update(self, item):
+        change = 2 if item.sell_in <= 0 else 1
+        self._inc(item, change)
+        item.sell_in -= 1
+
+class BackstagePassesUpdater(Updater):
+    def update(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        else:
+            if item.sell_in < 6:
+                self._inc(item, 3)
+            elif item.sell_in < 11:
+                self._inc(item, 2)
+            else:
+                self._inc(item, 1)
+        item.sell_in -= 1
