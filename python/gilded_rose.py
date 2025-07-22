fda7f1d8c678df2756a190616d22c33f96f3cdb6
Saad Zia <msaadzia7@gmail.com>
2025-07-22 14:05:00 +0100

    Replace strings with predefined constants

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -23,25 +23,25 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name not in ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"]:
+            if item.name not in [AGED_BRIE, BACKSTAGE_PASSES, SULFURAS]:
                 decrement_quality(item)
             else:
                 if item.quality < 50:
                     increment_quality(item)
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == BACKSTAGE_PASSES:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 increment_quality(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 increment_quality(item)
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != AGED_BRIE:
+                    if item.name != BACKSTAGE_PASSES:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != SULFURAS:
                                 decrement_quality(item)
                     else:
                         decrement_quality(item, item.quality)
